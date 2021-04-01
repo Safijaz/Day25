bb35016209cca05e4383fa1a89ee2e2bb3932b10
Safijaz <safija.zahirovic@gmail.com>
2021-04-01 22:29:41 +0200

    commit

---
+++ ex05/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequest from './Postrequest';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <PostRequest />
+     
+    </div>
+  );
+}
+
+export default App;
